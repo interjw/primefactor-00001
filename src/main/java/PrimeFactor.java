badee167c1bb111ad2b263268fbab282f376dd8f
Mac <kfc@gmail.com>
2025-06-04 10:40:35 +0900

    [refactor] 51p

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -5,22 +5,15 @@ public class PrimeFactor {
     public List<Integer> of(int number){
         List<Integer> factors = new ArrayList<>();
         if( number > 1  ) {
-            int devisor = 2;
-            if (number == 4) {
-                while (number % devisor == 0) {
-                    factors.add(devisor);
-                    number /= devisor;
-                }
-            }else if(number == 6){
-                for(devisor=2;number >1;devisor++){
+          int devisor = 2;
+
+          if(number == 4 || number == 6 || number == 9) {
+                for (devisor = 2; number > 1; devisor++) {
                     while (number % devisor == 0) {
                         factors.add(devisor);
                         number /= devisor;
                     }
                 }
-            }else if(number == 9) {
-                factors.add(3);
-                factors.add(3);
             }else {
                 factors.add(number);
             }
