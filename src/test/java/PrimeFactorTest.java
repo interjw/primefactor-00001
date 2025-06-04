6a11373c10ead95b3c63abab56f2ed0d5a54048e
Mac <kfc@gmail.com>
2025-06-04 10:17:17 +0900

    feature/24p

--- src/test/java/PrimeFactorTest.java
+++ src/test/java/PrimeFactorTest.java
@@ -1,11 +1,20 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class PrimeFactorTest {
 
     @Test
     void factor() {
+        PrimeFactor pf = new PrimeFactor();
         assertEquals(1,1);
+    }
+
+    @Test
+    void testPrimefactorOf1() {
+        PrimeFactor primefactor = new PrimeFactor();
+        assertEquals(Arrays.asList(),primefactor.of(1));
     }
 }
