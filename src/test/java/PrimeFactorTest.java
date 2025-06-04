5edf0569c0353c051a5249793109a535ab5888cd
Mac <kfc@gmail.com>
2025-06-04 09:58:47 +0900

    first commit

---
+++ src/test/java/PrimeFactorTest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimeFactorTest {
+
+    @Test
+    void factor() {
+        assertEquals(1,1);
+    }
+}
