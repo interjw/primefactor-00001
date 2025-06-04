cce3950064513c7aedc137306890886a87e6834d
Mac <kfc@gmail.com>
2025-06-04 10:31:36 +0900

    [refactor] 40p

--- src/main/java/PrimeFactor.java
+++ src/main/java/PrimeFactor.java
@@ -4,14 +4,21 @@ import java.util.List;
 public class PrimeFactor {
     public List<Integer> of(int number){
         List<Integer> factors = new ArrayList<>();
-        if( number > 1  ){
-            if(number == 4){
-                factors.add(2);
-                factors.add(2);
-            }else {
+        if( number > 1  ) {
+            if (number == 4) {
+                if (number % 2 == 0) {
+                    factors.add(2);
+                    number /= 2;
+                }
+                if (number % 2 == 0) {
+                    factors.add(2);
+                    number /= 2;
+                }
+            } else {
                 factors.add(number);
             }
         }
+
         return factors;
     }
 }
